f36737a91c0920875e8e2d301a39ce580f7c1f15
MiguelAngel <miguelangeladrianlizarmenavarr@gmail.com>
2026-05-31 21:40:14 -0500

    fix: Backend -- config

--- backend/src/main/java/com/UTP/PC02/config/CorsConfig.java
+++ backend/src/main/java/com/UTP/PC02/config/CorsConfig.java
@@ -36,8 +36,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(
+                    "http://localhost:4200",
+                    "https://exercises-alpha.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
